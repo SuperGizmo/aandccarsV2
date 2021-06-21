0a18bad791f1eb8e245a46f6dfe34eceebebe6da
Daniel Lambert <Daniel@grandc.co.uk>
2021-06-21 08:01:37 +0100

    Revising heading colours

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Welcome to A and C Cars Chertsey Taxi')
 
 @section('description', 'A and C Cars are based in Chertsey Surrey. We operate from Chertsey but we cover Surrey and Middlesex. We are reliable and fully licensed within Runnymede Council.')
+
 @section('keywords', 'A and C Cars, Chertsey Taxi, Addlestone Taxi, Surrey Taxi, Runnymede Taxi')
 
 @section('content')
