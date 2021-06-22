27c6f4cf8a824f870f9359184fd55fd6cb1aa533
Daniel Lambert <Daniel@grandc.co.uk>
2021-06-22 08:24:12 +0100

    Revising from address

--- app/Notifications/BookingReceived.php
+++ app/Notifications/BookingReceived.php
@@ -43,6 +43,7 @@ class BookingReceived extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
+            ->from('Website Booking <bookings@aandccars.co.uk>')
             ->subject('New Booking Enquiry')
             ->greeting('New Booking Enquiry')
             ->line('Here are your booking details:')
